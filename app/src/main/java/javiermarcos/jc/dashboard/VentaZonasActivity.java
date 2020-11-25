683284482938b3989a1c23b3258b2d92c0692900
JavierNezB <javier.nez.b@gmail.com>
2020-11-24 22:39:53 -0600

    Main activity con cardview

---
+++ app/src/main/java/javiermarcos/jc/dashboard/VentaZonasActivity.java
@@ -0,0 +1,14 @@
+package javiermarcos.jc.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VentaZonasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venta_zonas);
+    }
+}
---
+++ app/src/main/java/javiermarcos/jc/dashboard/ventasAxoActivity.java
@@ -0,0 +1,14 @@
+package javiermarcos.jc.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ventasAxoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventas_axo);
+    }
+}
