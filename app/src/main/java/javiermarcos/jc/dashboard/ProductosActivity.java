51b652fdf95c6bd037a7ae79010c180f73581e9c
JavierNezB <javier.nez.b@gmail.com>
2020-11-29 20:40:11 -0600

    Se termino el activity de Prodcutos

--- app/src/main/java/javiermarcos/jc/dashboard/ProductosActivity.java
+++ app/src/main/java/javiermarcos/jc/dashboard/ProductosActivity.java
@@ -3,12 +3,23 @@ package javiermarcos.jc.dashboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ProductosActivity extends AppCompatActivity {
+
+    ListView listaProductos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
+
+        listaProductos = (ListView) findViewById(R.id.listv1);
+
+        ArrayAdapter<CharSequence> adaptador = ArrayAdapter.createFromResource(this, R.array.arrayProdcutos,
+                R.layout.row);
+
+        listaProductos.setAdapter(adaptador);
     }
 }
