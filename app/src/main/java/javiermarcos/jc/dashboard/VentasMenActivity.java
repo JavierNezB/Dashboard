b0398f65d7a19670a56ddceb462b6edd761b5c36
JavierNezB <javier.nez.b@gmail.com>
2020-12-02 17:47:09 -0600

    Ya quedo la segunda grafica, seguire con las demas, y aqui QUITO LA GRAFICA DE PASTEL

--- app/src/main/java/javiermarcos/jc/dashboard/VentasMenActivity.java
+++ app/src/main/java/javiermarcos/jc/dashboard/VentasMenActivity.java
@@ -28,6 +28,8 @@ public class VentasMenActivity extends AppCompatActivity {
 
     private PieChart pieChart;
     private BarChart barChart1;
+    private BarChart barChart2;
+
     //datos del eje horizontal eje X
     private String [] months1 = new  String[]{"Ene","Feb","Mar","Abr","May", "Jun", "Jul","Ago","Sep","Oct","Nov","Dici"};
     //datos del eje vertical eje Y
@@ -45,9 +47,12 @@ public class VentasMenActivity extends AppCompatActivity {
 
         //Buscamos nuestras graficas en las vistas
         barChart1=(BarChart) findViewById(R.id.barchart1);
+        barChart2=(BarChart) findViewById(R.id.barchart02);
         //pieChart=(PieChart) findViewById(R.id.piechart);
         createChats();
+        createChats2();
     }
+
     //Personalizar la grafica datos generales
     private Chart getSameChart (Chart chart, String description, int text, int background, int animatey){
         //descripción de la grafica
@@ -177,4 +182,118 @@ public class VentasMenActivity extends AppCompatActivity {
         return new PieData(pieDataSet);
     }
      */
+
+    //***********************************
+    //************SEGUNDA GRAFICA********
+    //***********************************
+
+    //datos del eje horizontal eje X
+    //private String [] months1 = new  String[]{"Ene","Feb","Mar","Abr","May", "Jun", "Jul","Ago","Sep","Oct","Nov","Dici"};
+    //datos del eje vertical eje Y
+    private int [] sale2 = new int[]{3750000, 4100000, 3450000, 2800000, 4150000, 4500000, 3850000, 5200000, 4550000, 3900000, 3250000, 6600000};
+    //Colores de nuestra gafica
+    //private int [] colors1 = new int[]{Color.rgb(67, 221,242), Color.rgb(67, 221,242), Color.rgb(67, 221,242), Color.rgb(67, 221,242),
+    //       Color.rgb(67, 221,242), Color.rgb(67, 221,242), Color.rgb(67, 221,242), Color.rgb(67, 221,242),Color.rgb(67, 221,242),
+    //        Color.rgb(67, 221,242),Color.rgb(67, 221,242),Color.rgb(67, 221,242)};
+
+
+    //Personalizar la grafica datos generales
+    private Chart getSameChart2 (Chart chart2, String description, int text, int background, int animatey2){
+        //descripción de la grafica
+        chart2.getDescription().setText(description);
+        //color de la descreipción
+        chart2.getDescription().setTextColor(text);
+        //tamaño de la descripción
+        chart2.getDescription().setTextSize(15);
+        //color de fondo
+        chart2.setBackgroundColor(background);
+        //animación de la grafica
+        chart2.animateY(animatey2);
+
+        legend2(chart2);
+
+        return chart2;
+    }
+    //leyenda de la grafica
+    private void legend2 (Chart chart2){
+        //personalizar la leyenda
+        Legend legend2 =chart2.getLegend();
+        //personalizar los cuadros de colores
+        legend2.setForm(Legend.LegendForm.CIRCLE);
+        //se coloca centrado para la parte de abajo
+        legend2.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+
+        //rellenar los datos de la grafica
+        ArrayList<LegendEntry>entries2=new ArrayList<>();
+        for(int i = 0; i<months1.length; i++){
+            //variable para agregar los datos de la leyenda
+            LegendEntry entry2 = new LegendEntry();
+            entry2.formColor=colors1[i];
+            entry2.label=months1[i];
+            entries2.add(entry2);
+        }
+        legend2.setCustom(entries2);
+    }
+    //datos dentro de nuestra gafica de barras
+    private ArrayList<BarEntry>getBarEntries2(){
+        ArrayList<BarEntry> entries2 = new ArrayList<>();
+        for (int i = 0; i<sale2.length; i++){
+            entries2.add(new BarEntry(i, sale2[i]));
+        }
+        return entries2;
+    }
+    //agregar cada uno de los valores, seria la barra
+    private void axisX2(XAxis axis2){
+        axis2.setGranularityEnabled(true);
+        axis2.setPosition(XAxis.XAxisPosition.BOTTOM);
+        axis2.setValueFormatter(new IndexAxisValueFormatter(months1));
+        //aqui se habilita las etiquetas en la parte inferior
+        axis2.setEnabled(true);
+    }
+    private void axisLeft2(YAxis axis2){
+        //espacio de arriba
+        axis2.setSpaceTop(30);
+        //valor minimo eje Y
+        axis2.setAxisMinimum(0);
+        //cambiar gradualidad
+        axis2.setGranularity(1500000);
+    }
+    private void axisRight2(YAxis axis2){
+        // poner numeros del lado derecho
+        axis2.setEnabled(false);
+    }
+    //creación de la grafica de barras
+    public void createChats2(){
+        barChart2 = (BarChart) getSameChart2(barChart2, "Ventas 2020 Alvaro Obregon", Color.BLACK, Color.TRANSPARENT, 3000);
+        //carqacteristicas de la grafica:
+        barChart2.setDrawGridBackground(false);// quitamos el fondo feo detras de la grafica
+        barChart2.setDrawBarShadow(false); // activamos la sobra restante de la grafica
+        barChart2.setData(getBarData2());
+        barChart2.invalidate();
+        axisX2(barChart2.getXAxis());
+        axisLeft2(barChart2.getAxisLeft());
+        axisRight2(barChart2.getAxisRight());
+        //Poner en la grafica de PIE los indicadores arriba
+        barChart2.getLegend().setEnabled(false);
+    }
+
+    //agregar los datos dentro de la grafica
+    private DataSet getData2(DataSet dataSet2){
+        dataSet2.setColors(colors1);
+        //Cambiar color de los numeros dentro de la grafica las etiquetas
+        dataSet2.setValueTextColor(Color.BLACK);
+        //tamaño del texto
+        dataSet2.setValueTextSize(10);
+        return dataSet2;
+    }
+    //personalizar el contenido la grafica de BARRAS
+    private BarData getBarData2(){
+        BarDataSet barDataSet2 =(BarDataSet)getData2(new BarDataSet(getBarEntries2(), ""));
+
+        barDataSet2.setBarShadowColor(Color.TRANSPARENT);
+        BarData barData2 = new BarData(barDataSet2);
+        //ancho de las barras
+        barData2.setBarWidth(0.45f);
+        return barData2;
+    }
 }
